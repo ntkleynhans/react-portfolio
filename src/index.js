81b434625a309153404f16ad267b41d4ea14bd95
Neil Kleynhans <ntkleynhans@gmail.com>
2019-04-01 18:02:45 +0200

    adding animals

--- src/index.js
+++ src/index.js
@@ -3,3 +3,31 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
+
+class Animal {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  speak() {
+    console.log('I am ', this.name, 'and I am ', this.age, 'years old');
+  }
+}
+
+const animal = new Animal('Simba', 3);
+animal.speak();
+
+class Lion extends Animal {
+  constructor(name, age, furColor, speed) {
+    super(name, age);
+    this.furColor = furColor;
+    this.speed = speed;
+  }
+  roar() {
+    console.log('ROAR!!! I have ', this.furColor, 'and I run this fast', this.speed);
+  }
+}
+
+const lion = new Lion('Mufasa', 20, 'golden', 25);
+lion.speak();
+lion.roar();
