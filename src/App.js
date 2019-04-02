8d7587256d7f935b8e3c6a0ddaf0d4f9ad256c5d
Neil Kleynhans <ntkleynhans@gmail.com>
2019-04-02 13:38:18 +0200

    toggle state bio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { displayBio: false};
+
+    this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
+  }
+
+  toggleDisplayBio() {
+    this.setState({ displayBio: !this.state.displayBio})
+  }
+
   render() {
+
     return(
       <div>
         <h1>Hello!</h1>
         <p>My name is Neil. I'm a software engineer.</p>
+        {this.state.displayBio ? (<div>
+          <p>I live in Cape Town</p>
+          <p>I'm an engineer that likes to code</p>
+          <p>What is the the ultimate app</p>
+          <button onClick={this.toggleDisplayBio}>Show less</button>
+        </div>
+        ) :  (<div>
+          <button onClick={this.toggleDisplayBio}>Read More...</button>
+        </div> )}
       </div>
     )
   }
