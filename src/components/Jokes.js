1b6ede516688597f35dc8dfe5d9957190341fddd
Neil Kleynhans <ntkleynhans@gmail.com>
2019-04-10 14:00:16 +0200

    fetch api with promises

--- src/components/Jokes.js
+++ src/components/Jokes.js
@@ -1,20 +1,41 @@
 import React, { Component } from 'react';
 
+const Joke = ({ joke : { setup, punchline} }) => (
+  <p>{setup} <em>{punchline}</em></p>
+)
+
 class Jokes extends Component {
-  state = { joke : {}, };
+  state = { joke : {}, jokes : [] };
 
   componentDidMount() {
     fetch("https://official-joke-api.appspot.com/random_joke")
     .then(response => response.json())
-    .then(json => this.setState({ joke : json}));
+    .then(json => this.setState({ joke : json}))
+    .catch(error => alert(error.message));
+
+    this.refreshTenJokes();
+  }
+
+  refreshTenJokes = () => {
+    fetch("https://official-joke-api.appspot.com/random_ten")
+    .then(response => response.json())
+    .then(json => this.setState({ jokes : json}))
+    .catch(error => alert(error.message));
   }
 
   render() {
-    const { setup, punchline } = this.state.joke;
+    const jokes = this.state.jokes;
     return (
       <div>
         <h2>Highlighted Joke</h2>
-        <p>{setup} <em>{punchline}</em></p>
+        <Joke joke={this.state.joke} />
+        <hr />
+        <button type="button" className="btn btn-info" onClick={this.refreshTenJokes}>Ten new jokes...</button>
+        {
+          jokes.map(joke => (
+            <Joke key={joke.id} joke={joke} />
+          ))
+        }
       </div>
     )
   }
